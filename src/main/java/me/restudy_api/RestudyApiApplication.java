3e7007bef7c0feb0c7ec6774e862ead4c5f38620
Luís Coelho <luis.coelho.761@gmail.com>
2025-04-21 16:48:53 -0300

    Iniciando projeto restudy-api

---
+++ src/main/java/me/restudy_api/RestudyApiApplication.java
@@ -0,0 +1,13 @@
+package me.restudy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestudyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestudyApiApplication.class, args);
+	}
+
+}
